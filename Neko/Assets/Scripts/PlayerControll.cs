f5fe2e82e9297d769360f59f1d5fcc1dcdf0a2b4
isizaki mituru <k019g1347isizaki@gmail.com>
2020-10-27 01:30:34 +0900

    はしご作成 スイッチ１作成

--- Neko/Assets/Scripts/PlayerControll.cs
+++ Neko/Assets/Scripts/PlayerControll.cs
@@ -2,21 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum State
+{
+    Nuetral,
+    Ladder
+}
+
 public class PlayerControll : MonoBehaviour
 {
     private Rigidbody rigidbody;
     [SerializeField] private float speed = 3;
 
+    private State state;
+
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+
+        state = State.Nuetral;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        rigidbody.velocity = new Vector3(x * speed, 0, 0);
+        switch (state)
+        {
+            case State.Nuetral:
+                float x = Input.GetAxis("Horizontal");
+                rigidbody.velocity = new Vector3(x * speed, rigidbody.velocity.y, 0);
+                rigidbody.AddForce(0, -0.5f, 0);
+                rigidbody.useGravity = true;
+
+                break;
+            case State.Ladder:
+                float y = Input.GetAxis("Vertical");
+                rigidbody.velocity = new Vector3(0, y * speed, 0);
+                rigidbody.useGravity = false;
+
+                break;
+        }
+
+    }
+
+
+    public void ChangeState(State newState)
+    {
+        state = newState;
     }
 }
---
+++ Neko/Assets/Scripts/PlayerFloor.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFloor : MonoBehaviour
+{
+    private PlayerControll parent;
+    private GameObject ladder2; // ボタンで出現するはしご
+    private GameObject switch1; // ボタンの上のビックリマーク
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        parent = GetComponentInParent<PlayerControll>();
+
+        // 非アクティブだとFindできない
+        ladder2 = GameObject.Find("Ladder2");
+        ladder2.SetActive(false);
+        switch1 = GameObject.Find("Switch_Tex");
+        switch1.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // はしご判定
+        if (other.gameObject.tag == "LadderBottom")
+        {
+            if (Input.GetAxis("Vertical") != 0)
+            {
+                parent.ChangeState(State.Ladder);
+            }
+            if (Input.GetAxis("Horizontal") != 0)
+            {
+                parent.ChangeState(State.Nuetral);
+            }
+        }
+        if (other.gameObject.tag == "LadderTop")
+        {
+            parent.ChangeState(State.Nuetral);
+        }
+        if (other.gameObject.tag == "Ladder")
+        {
+            parent.ChangeState(State.Ladder);
+        }
+
+        // ボタン判定
+        if (other.gameObject.tag == "Switch")
+        {
+            // はしごが非アクティブなら表示する
+            if (ladder2.activeSelf == false)
+            {
+                switch1.SetActive(true);
+            }
+
+            // スペースキーではしご出現（一旦）
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ladder2.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Switch")
+        {
+            // ボタンから離れたら非表示
+            switch1.SetActive(false);
+        }
+    }
+}
