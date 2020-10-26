c7769c3a50175ea9111d403bdca4dbbcd686275a
isizaki mituru <k019g1347isizaki@gmail.com>
2020-10-26 11:41:38 +0900

    仮ステージの配置 Player Prefab作成

---
+++ Neko/Assets/Scripts/PlayerControll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControll : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    [SerializeField] private float speed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector3(x * speed, 0, 0);
+    }
+}
